7f55010f9a116c612ef266f9f044f864ca32eb63
ALIX8513 <I213ALG@GMAIL.COM>
2017-07-17 16:16:36 +0100

    Add files via upload

---
+++ Home.tv.php
@@ -0,0 +1,40 @@
+{"HDvideo":[
+	{
+	 "id":"100",
+	 "cat_id":"44",
+	 "video_title":"beIN News",
+	 "video_url":"https://www.youtube.com/watch?v=9A6j2L2fEM8",
+	 "video_id":"9A6j2L2fEM8",
+	 "video_thumbnail":"",
+	 "video_duration":"Sport",
+	 "video_description":"Descarga la version pro en la parte superior izquierda en los 3 puntos es gratis, consultas whatsapp +50259869346",
+	 "cid":"44",
+	 "category_name":"beIN News",
+	 "category_image":"67610_14783_categoria.jpg"
+	 },
+	 	{
+	 "id":"101",
+	 "cat_id":"44",
+	 "video_title":"Al Jazeera Arabic",
+	 "video_url":"https://www.youtube.com/watch?v=fN2sEo7hq-E",
+	 "video_id":"fN2sEo7hq-E",
+	 "video_thumbnail":"",
+	 "video_duration":"News",
+	 "video_description":"Descarga la version pro en la parte superior izquierda en los 3 puntos es gratis, consultas whatsapp +50259869346",
+	 "cid":"44",
+	 "category_name":"Al Jazeera Arabic",
+	 "category_image":"67610_14783_categoria.jpg"
+	 	 },
+	 	{
+	 "id":"102",
+	 "cat_id":"44",
+	 "video_title":"Al Arabiya",
+	 "video_url":"https://www.youtube.com/watch?v=U01Rd-4wAB0",
+	 "video_id":"U01Rd-4wAB0",
+	 "video_thumbnail":"",
+	 "video_duration":"News",
+	 "video_description":"Descarga la version pro en la parte superior izquierda en los 3 puntos es gratis, consultas whatsapp +50259869346",
+	 "cid":"44",
+	 "category_name":"Al Arabiya",
+	 "category_image":"67610_14783_categoria.jpg"}]}
+	]}
